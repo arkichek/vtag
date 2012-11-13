cb9051303e7c96a13d194238857412ce28a069fd
Arkady Fraerman <arkady.fr@gmail.com>
2012-11-13 19:17:58 +0400

    Парсинг аргументов

--- vtag.py
+++ vtag.py
@@ -2,5 +2,25 @@
 # -*- coding: utf-8 -*-
 # 2012 (c) Arkady Fraerman <arkady.fr@gmail.com>
 
+
+import argparse
+
+
+class VersionTag:
+	def __init__(self, projPath):
+		self.projPath = projPath
+		pass
+
+
+
+def parseArgs():
+	parser = argparse.ArgumentParser(description='Tool for managing version labels in iOS projects')
+	parser.add_argument('-t', '--tag', dest='tag', help='Human version label')
+	parser.add_argument('-b', '--build', dest='build', help='New build number')
+	parser.add_argument('-ib', '--incrementBuild', dest='incrementBuild', action='store_true', default=False, help='Increase by one step build number')
+	parser.add_argument('projPath', help='Path to .xcodeproj file')
+	return parser.parse_args()
+
+
 if __name__ == '__main__':
-	pass
+	args = parseArgs()
